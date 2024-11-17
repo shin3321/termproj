c066be0b1fa299c0b3f49afc0c1b9b4ce8a6298a
parkshinehye <qkrtlsgp0996@gmail.com>
2024-11-17 23:21:16 +0900

    스테이지2

---
+++ stage2.py
@@ -0,0 +1,10 @@
+from pico2d import  *
+
+
+class Map2:
+    def __init__(self):
+        self.image = load_image('img/Jungle_Tiles.png')
+        self.x, self.y = 0,0
+
+    def draw(self):
+        self.image.clip_draw()
